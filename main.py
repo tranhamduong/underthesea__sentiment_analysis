d098d3133cdfce58c0e27676752d599b7792181f
duongth <duongtranham@admicro.vn>
2020-10-10 18:53:46 +0700

    update .xlsx file

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import pandas as pd
 from underthesea import sentiment
+from underthesea import classify
 
 
-file = "/home/duongth/personal/preprocess/git/preprocess_word/dataset/maybe_negative.csv"
-
-df = pd.read_csv(file)
-
-df['sentiment'] = df['data'].apply(sentiment)
-
-df.to_csv('maybe_negative_sentiment.csv')
 
 
-file = "/home/duongth/personal/preprocess/git/preprocess_word/dataset/unknown.csv"
-
-df = pd.read_csv(file)
-
-df['sentiment'] = df['data'].apply(sentiment)
-
-df.to_csv('unknown.csv')
+# file = "/home/duongth/personal/preprocess/git/preprocess_word/dataset/maybe_negative.csv"
+#
+# df = pd.read_csv(file)
+#
+# df['sentiment'] = df['data'].apply(sentiment)
+#
+# df.to_csv('maybe_negative_sentiment.csv')
+#
+#
+# file = "/home/duongth/personal/preprocess/git/preprocess_word/dataset/unknown.csv"
+#
+# df = pd.read_csv(file)
+#
+# df['sentiment'] = df['data'].apply(sentiment)
+predict = sentiment('hello world')
+b = classify('Ủng hộ chính phủ')
+print(b)
+# df.to_csv('unknown.csv')
